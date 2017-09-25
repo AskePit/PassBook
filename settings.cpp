f9c09cea0339aae10a411cc6d3db5b11127b9920
Nikolay Shalakin <nikolay.shalakin@astrosoft.ru>
2017-09-25 16:53:59 +0300

    1.1.0 version

--- settings.cpp
+++ settings.cpp
@@ -42,7 +42,7 @@ Language::type Language::fromi18nName(const QString &str)
 }
 
 Settings::Settings()
-    : version(1, 0, 0)
+    : version(1, 1, 0)
     , language(Language::English)
     , accountsPath("")
     , translator(nullptr)
