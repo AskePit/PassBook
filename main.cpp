bef357dd97a09b91df6db7775ce452974f31eb4a
Nikolai Shalakin <nikolai.shalakin@lifeisfeudal.com>
2024-09-29 13:55:57 +0200

    apply Fusion style

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <QApplication>
+#include <QStyleFactory>
 #include "dialogs/logindialog.h"
 
 int main(int argc, char *argv[])
 {
+    QApplication::setStyle(QStyleFactory::create(QStringLiteral("Fusion")));
     QApplication a(argc, argv);
 
     LoginDialog *w { new LoginDialog };
